6db216dbd585e8232dbae697086b59908fded9f3
Ryan O'Dell <110799654+Ryano756364@users.noreply.github.com>
2023-02-20 15:16:09 -0500

    LinkedList add | sort | compareTo | iterator

--- src/main/java/_02_collections/_04_linked_list.java
+++ src/main/java/_02_collections/_04_linked_list.java
@@ -1,31 +1,39 @@
 package _02_collections;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class _04_linked_list {
     public static void main(String[] args) {
         LinkedList<String> stores = new LinkedList<>();
-        stores.add("Best Buy");
         stores.add("Microcenter");
         stores.add("Target");
         stores.add("UPS");
-
+        stores.add("Best Buy");
         printLinkedList(stores);
 
-        stores.add(1, "Best Buy 2");
-
+        stores.add(1, "Best Buy Second Store");
         printLinkedList(stores);
 
         stores.set(0, "Circuit City");
-
         printLinkedList(stores);
-
 
         //This strategy of initializing is recommended - but watch performance issues on which type of list to use
         List<String> employees = new LinkedList<>();  // good for fast operations
         List<String> states = new ArrayList<>();  // good for fast access
+
+        stores.sort(null);  //null will base ordering based off natural ordering
+        printLinkedList(stores);
+
+        Collections.reverse(stores);
+        printLinkedList(stores);
+
+        //If part 1 is greater than part 2 -> positive number returned
+        //If equal, 0 is returned
+        //If part 2 is greater than part 1 -> negative number returned
+        System.out.println(stores.get(3).compareTo(stores.get(4)));
+        //Best Buy Second Store COMPARE TO Best Buy
+        //13 is returned
+        //Can only compare with objects; no primitive types
     }
 
     //Could also set up incoming parameter as 'LIST' to allow for more types of String lists to be sent in
@@ -33,6 +41,24 @@ public class _04_linked_list {
     public static void printLinkedList(LinkedList<String> linkedList){
         for(String e: linkedList){
             System.out.println(e);
+        }
+        System.out.println("******");
+    }
+
+    //Iterator versus For Each doesn't have much of a difference
+    public static void iterateLinkedList(List<String> list){
+        Iterator<String> iterator = list.listIterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator);
+        }
+        System.out.println("******");
+    }
+
+    //Benefit is if we need to move to previous element in list
+    public static void listIterateOverLinkedList(List<String> list){
+        ListIterator<String> iterator = list.listIterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator);
         }
         System.out.println("******");
     }
