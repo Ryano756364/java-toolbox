11c1cd7337488b40dc2abff832ccfef1a086f9ce
Ryan O'Dell <110799654+Ryano756364@users.noreply.github.com>
2023-02-20 18:10:16 -0500

    Create new directory

--- src/main/java/_03_file_handling/_01_create_new_file.java
+++ src/main/java/_03_file_handling/_01_create_new_file.java
@@ -6,7 +6,11 @@ import java.io.IOException;
 public class _01_create_new_file {
 
     public static void main(String[] args) throws IOException {
-        File file = new File("src/CustomerList.txt");
+        File directory = new File("src/db");
+        directory.mkdir();
+        System.out.println("Directory created!");
+
+        File file = new File(directory + "/CustomerList.txt");
         file.createNewFile();
         System.out.println("File created!");
     }
